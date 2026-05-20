d70cf74438a3e4f4a13970beb08917be90cc9bd2
Yash Pippal <xenoxander9274@gmail.com>
2026-05-20 18:41:56 +0530

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+    if (x < 0) return false;
+    if (x == 0) return true;
+
+    int orig = x;  
+
+    int n = 0;
+    while (x > 0) {  
+        x /= 10;
+        n++;
+    }
+
+    int A[n];
+    for (int i = 0; i < n; i++) {
+        A[i] = orig % 10;  
+        orig /= 10;
+    }
+
+    for (int j = 0; j < n; j++) {
+        if (A[j] != A[n - 1 - j])
+            return false;  
+    }
+    return true;  
+}
+};
