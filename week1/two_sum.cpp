e1e251c9ae2afe0aa9dd392c24155d1d66d50772
Yash Pippal <xenoxander9274@gmail.com>
2026-07-11 23:28:24 +0530

    Update two_sum.cpp

--- week1/two_sum.cpp
+++ week1/two_sum.cpp
@@ -3,29 +3,29 @@
 
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        for(int i = 1; i < nums.size(); i++){
-            for(int j = 0; j < i; j++){
-            if(nums[i] + nums[j] == target){ 
-                    return {j, i};
-            }
-        }
-   }
-        return {};
-    }
-};
-
-// Better solution using hash map: O(n) time complexity and O(n) space complexity
-// std::vector<int> twoSum(std::vector<int>& nums, int target) {
-//     std::unordered_map<int, int> seen;   // value → index
-
-//     for (int i = 0; i < nums.size(); i++) {
-//         int complement = target - nums[i];
-
-//         if (seen.count(complement))         // complement seen before?
-//             return {seen[complement], i};   // return its index and current index
-
-//         seen[nums[i]] = i;                  // store current value → index
+//     std::vector<int> twoSum(std::vector<int>& nums, int target) {
+//         for(int i = 1; i < nums.size(); i++){
+//             for(int j = 0; j < i; j++){
+//             if(nums[i] + nums[j] == target){ 
+//                     return {j, i};
+//             }
+//         }
+//    }
+//         return {};
 //     }
-//     return {};
-// }
+
+//Better solution using hash map: O(n) time complexity and O(n) space complexity
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> seen;   // value → index
+
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+
+        if (seen.count(complement))         // complement seen before?
+            return {seen[complement], i};   // return its index and current index
+
+        seen[nums[i]] = i;                  // store current value → index
+    }
+    return {};
+}
+};
